bc13a45d45799a552be0ece034502e11b5c892a2
liuhu <1321947437@qq.com>
2021-10-23 23:17:22 +0800

    urldemo1

---
+++ src/main/java/startMain.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class startMain {
+
+    public static void main(String[] args) throws IOException {
+        //最基础语句 控制台输出hello
+        System.out.println("hello");
+        //调用urlDao类测试程序执行效果
+        urldemo1 urldemo1=new urldemo1();//::java: 错误: 无效的源发行版：16
+
+    }
+}
---
+++ src/urlDemo/urldemo1.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**初始化构造方法 由于比较简单直接在构造中写程序
+ * 该类是学习用java链接到服务器的demo
+ * */
+public class urldemo1 {
+    //Socket处理异常
+    public urldemo1() throws IOException {
+        //打开一个套接字，一旦套接字打开就会得到一个stream
+       try(Socket s =new Socket("time-a.nist.gov",13);
+           Scanner in =new Scanner(s.getInputStream(),"UTF-8"))
+        {
+           while(in.hasNextLine()){
+               String line=in.nextLine();
+               System.out.println(line);
+           }
+        }
+
+    }
+}
